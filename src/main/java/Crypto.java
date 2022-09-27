ce0d965d7563264c2f813b3fe35b9fbea1e3b3fd
H-SChan <dlwp7581@gmail.com>
2022-09-27 10:56:29 +0900

    [ADD] annotation for Method and Class

--- src/main/java/Crypto.java
+++ src/main/java/Crypto.java
@@ -1,13 +1,31 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * can SHA, MD
+ */
 public class Crypto {
+    /**
+     * encode a string
+     * @param algorithm want algorithm in SHA or MD
+     * @param str want to encode
+     * @return encoded string what argument str to argument algorithm
+     * @throws NoSuchAlgorithmException can't find algorithm or Illegal algorithm
+     */
     public String encode(String algorithm, String str) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance(algorithm);
         md.update(str.getBytes());
         return bytesToHex(md.digest());
     }
 
+    /**
+     * hashed string is same to target string
+     * @param algorithm an algorithm used for hash string encoding
+     * @param target Expected string as source of encoded string
+     * @param hash encoded string
+     * @return is same target string encoded to algorithm with hash
+     * @throws NoSuchAlgorithmException can't find algorithm or Illegal algorithm
+     */
     public boolean decode(String algorithm, String target, String hash) throws NoSuchAlgorithmException {
         return this.encode(algorithm, target).equals(hash);
     }
